ebabd4b09aa7e9aa37d389ea6a9e22d30b2fd6e8
yoojj1990 <yoojj1990@gmau>
2022-07-12 14:45:19 +0900

    수정

--- src/main/java/com/yjj/jpatest/MemberDto.java
+++ src/main/java/com/yjj/jpatest/MemberDto.java
@@ -28,6 +28,30 @@ public class MemberDto {
 		this.id = id;
 		this.name = name;
 	}
+
+	public Long getNum() {
+		return num;
+	}
+
+	public void setNum(Long num) {
+		this.num = num;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
 	
 	
 	
--- src/main/java/com/yjj/jpatest/MemberService.java
+++ src/main/java/com/yjj/jpatest/MemberService.java
@@ -2,6 +2,7 @@ package com.yjj.jpatest;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -18,8 +19,38 @@ public class MemberService {
 		return members;
 	}
 	
-	public void deleteById(Long num) {
+	public void deleteById(Long num) {  // 특정 num으로 조회해서 삭제(delete)
 		memberRepository.deleteById(num);
 	}
+	
+	public Optional<MemberDto> findById(Long num) { // 특정 num으로 조회 (select)
+		
+		Optional<MemberDto> member = memberRepository.findById(num);
+		
+		return member;
+	}
+	
+	public MemberDto save(MemberDto member) {  // 새로운 member 삽입 (insert)
+		
+		memberRepository.save(member);
+		
+		return member;
+	}
+	
+	
+	public void updateById(Long num, MemberDto member) { // 데이터 수정 (update)
+		
+		Optional<MemberDto> member_temp = memberRepository.findById(num);
+		// 기존 해당 id의 데이터(dto) 불러오기
+		
+		if(member_temp.isPresent()) {
+			member_temp.get().setNum(member.getNum());
+			member_temp.get().setId(member.getId());
+			member_temp.get().setName(member.getName());
+			memberRepository.save(member);
+		} // 새로 수정된 내용으로 업데이트
+		
+	}
+	
 	
 }
